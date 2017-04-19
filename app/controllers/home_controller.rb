27a9f3d58dd8db6b8b79a736f83fce9174c8c618
Ashar <sarah.eggleston@alliants.com>
2017-04-19 16:55:24 +0100

    Rubocop fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,21 +5,9 @@ class HomeController < ApplicationController
   skip_authorization_check
 
   def show
-    # This is the space ID. A space is like a project folder in Contentful terms
-    space_id = 'developer_bookshelf'
-
-    # This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-    access_token = '0b7f6x59a0'
-
     client = ContentfulService.call
-    entry_id = '5PeGS2SoZGSa4GuiQsigQu'
+    entry_id = "5PeGS2SoZGSa4GuiQsigQu"
     @entry = client.entry(entry_id)
-
-    #puts "===================="
-    #puts "Contentful content"
-    #puts "#{entry.name} - by #{entry.author}"
-    #puts "\t  #{entry.description}"
-    #puts "===================="
 
     redirect_to(guest_dashboard_path) && return if current_user&.guest?
     redirect_to(admin_dashboard_path) && return if current_user
